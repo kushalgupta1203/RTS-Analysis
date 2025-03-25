ed8ef390e45d9eacba3e06132c2243f6aa6ed67c
Kushal Gupta <kushalgupta1203@gmail.com>
2025-03-25 23:23:44 +0530

    Update ydata_profiling_analysis.py

--- ydata_profiling_analysis.py
+++ ydata_profiling_analysis.py
@@ -2,10 +2,10 @@ import pandas as pd
 from ydata_profiling import ProfileReport
 
 # Define file location
-file_path = r"D:\Projects\RTS Analysis\consumer_preprocessed_data.xlsx"
+file_path = r"D:\Projects\RTS Analysis\consumer_cleaned_data.csv"
 
 # Read Excel file
-df = pd.read_excel(file_path)
+df = pd.read_csv(file_path)
 
 # Generate report
 profile = ProfileReport(df, title="Solar Data Analysis", explorative=True)
